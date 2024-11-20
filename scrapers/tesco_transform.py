cc53f30aeaf81a41fad37b6accef005e956eca71
Paula <paula.uusnakki@gmail.com>
2024-11-20 15:04:51 +0000

    Get unit name

--- scrapers/tesco_transform.py
+++ scrapers/tesco_transform.py
@@ -14,9 +14,15 @@ def get_unit_price_float(search_result: dict) -> float:
 
     return float(search_result["unit_price"].split("/")[0].replace("£", ""))
 
+def get_unit_name(search_result: dict) -> str:
+    """Returns the unit name for unit price."""
+
+    return search_result["unit_price"].split("/")[1]
+
 if __name__ == "__main__":
     data = [{'product_name': 'Tesco Classic Round Tomatoes 6 Pack', 'item_price': '£0.95', 'unit_price': '£0.16/each', 'loyalty_item_price': '£0.95', 'loyalty_unit_price': '£0.16/each',
             'product_image_url': 'https://digitalcontent.api.tesco.com/v2/media/ghs/f8a5bbce-3cf5-4c3e-ba28-d1f7963c59b1/dcabae4b-8259-4f95-a3ec-33335f88fe0e.jpeg?h=225&w=225'}, {
                 'product_name': 'Tesco Baby Plum Tomatoes 300G', 'item_price': '£1.00', 'unit_price': '£3.33/kg', 'loyalty_item_price': 'Save 25% 75p Clubcard Price', 'loyalty_unit_price': '(£2.50/kg)', 'product_image_url': 'https://digitalcontent.api.tesco.com/v2/media/ghs/479aeb6d-d911-44c3-8553-aade9b0d003c/8d0eeaf6-2c07-4766-8c5d-c3f8f84d6ca2.jpeg?h=225&w=225'}]
     print(change_item_price_to_float(data[1]))
     print(get_unit_price_float(data[1]))
+    print(get_unit_name(data[1]))
