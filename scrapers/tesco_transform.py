90408261906b0a5cb78bae5d140e0f3fb7c666a4
Paula <paula.uusnakki@gmail.com>
2024-11-20 14:56:33 +0000

    Get unit price float

--- scrapers/tesco_transform.py
+++ scrapers/tesco_transform.py
@@ -5,17 +5,18 @@ This is the transform script for scraping Tesco groceries from the shop's websit
 
 def change_item_price_to_float(search_result: dict) -> float:
     """Changes all the item prices in the result into a float."""
-    search_result["item_price"] = float(
-        search_result["item_price"].replace("£", ""))
 
-    return search_result
+    return float(search_result["item_price"].replace("£", ""))
 
 
 def get_unit_price_float(search_result: dict) -> float:
-    ...
+    """Changes unit price into a float."""
 
+    return float(search_result["unit_price"].split("/")[0].replace("£", ""))
 
 if __name__ == "__main__":
     data = [{'product_name': 'Tesco Classic Round Tomatoes 6 Pack', 'item_price': '£0.95', 'unit_price': '£0.16/each', 'loyalty_item_price': '£0.95', 'loyalty_unit_price': '£0.16/each',
-            'product_image_url': 'https://digitalcontent.api.tesco.com/v2/media/ghs/f8a5bbce-3cf5-4c3e-ba28-d1f7963c59b1/dcabae4b-8259-4f95-a3ec-33335f88fe0e.jpeg?h=225&w=225'}, {'product_name': 'Tesco Baby Plum Tomatoes 300G', 'item_price': '£1.00', 'unit_price': '£3.33/kg', 'loyalty_item_price': 'Save 25% 75p Clubcard Price', 'loyalty_unit_price': '(£2.50/kg)', 'product_image_url': 'https://digitalcontent.api.tesco.com/v2/media/ghs/479aeb6d-d911-44c3-8553-aade9b0d003c/8d0eeaf6-2c07-4766-8c5d-c3f8f84d6ca2.jpeg?h=225&w=225'}]
+            'product_image_url': 'https://digitalcontent.api.tesco.com/v2/media/ghs/f8a5bbce-3cf5-4c3e-ba28-d1f7963c59b1/dcabae4b-8259-4f95-a3ec-33335f88fe0e.jpeg?h=225&w=225'}, {
+                'product_name': 'Tesco Baby Plum Tomatoes 300G', 'item_price': '£1.00', 'unit_price': '£3.33/kg', 'loyalty_item_price': 'Save 25% 75p Clubcard Price', 'loyalty_unit_price': '(£2.50/kg)', 'product_image_url': 'https://digitalcontent.api.tesco.com/v2/media/ghs/479aeb6d-d911-44c3-8553-aade9b0d003c/8d0eeaf6-2c07-4766-8c5d-c3f8f84d6ca2.jpeg?h=225&w=225'}]
     print(change_item_price_to_float(data[1]))
+    print(get_unit_price_float(data[1]))
